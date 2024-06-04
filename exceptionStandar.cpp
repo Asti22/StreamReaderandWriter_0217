a698aea8f8075bdd8bf6bcb2a1fb756408ea0f5d
Asti22 <uun3461@gmail.com>
2024-06-04 15:05:42 +0700

    membuat int main

---
+++ exceptionStandar.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <exception>
+//untuk obyek exception yang akan digunkan 
+#include <array>
+//untuk obyek array yang akan kita gunakan 
+using namespace std;
+int main(){
+    cout <<"Awal program "<<endl; //penanda 1
+}

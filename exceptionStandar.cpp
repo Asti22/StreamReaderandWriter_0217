c51d851317a25ce879e65ded73fb548365aca2c7
Asti22 <uun3461@gmail.com>
2024-06-04 15:06:55 +0700

    membuat array dengan 3 panjang data

--- exceptionStandar.cpp
+++ exceptionStandar.cpp
@@ -6,4 +6,8 @@
 using namespace std;
 int main(){
     cout <<"Awal program "<<endl; //penanda 1
+    try {
+        array <int, 3> data = {3, 5, 7};
+        //pesan array integer 3 elemen
+    }
 }
